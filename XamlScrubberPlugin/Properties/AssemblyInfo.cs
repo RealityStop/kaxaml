0748fc5a3e067af4e883c2dfe50b62347c814c0c
jkuehner <jochen.kuehner@kardex.com>
2017-08-09 06:14:08 +0200

    AppVeyor Build

--- XamlScrubberPlugin/Properties/AssemblyInfo.cs
+++ XamlScrubberPlugin/Properties/AssemblyInfo.cs
@@ -17,7 +17,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("XamlScrubberPlugin")]
-[assembly: AssemblyCopyright("Copyright @  2007")]
+[assembly: AssemblyCopyright("Copyright @  2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
@@ -59,4 +59,4 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("2.0.*")]
